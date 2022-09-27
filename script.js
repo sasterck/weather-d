2c212c8ccced74350ebc3ac5a027082f2d12258e
[Sara Sterck] <[sasterck14@gmail.com]>
2022-09-27 16:42:14 -0600

    trying to get ajax to work

--- script.js
+++ script.js
@@ -10,12 +10,10 @@ var currentWindSpeed = $("#current-wind-speed");
 var UVindex = $("#uv-index");
 
 var weatherContent = $("#weather-content");
-
-// Get access to the OpenWeather API
-var APIkey = "a17e1499228be1f9c294ac18b234c7d7";
-
-// Easy access to data
 var cityList = [];
+// OpenWeather API
+var APIkey = "e5a600330dcc1a04f3349222a9a1a298";
+
 
 // Find current date and display in title
 var currentDate = moment().format('L');
@@ -25,18 +23,9 @@ $("#current-date").text("(" + currentDate + ")");
 initalizeHistory();
 showClear();
 
-// Hitting enter while input is focused will trigger
-// value added to search history
-$(document).on("submit", function(){
-    event.preventDefault();
 
     // Grab value entered into search bar 
     var searchValue = searchCityInput.val().trim();
-
-    currentConditionsRequest(searchValue)
-    searchHistory(searchValue);
-    searchCityInput.val(""); 
-});
 
 // Clicking the search button will trigger
 // value added to search history
